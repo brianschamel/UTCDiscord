8e1ee2a05370e80adcc7f99468b2861e798c776b
brianschamel <schamelb4@gmail.com>
2022-04-27 19:24:41 -0500

    should have everything necessary

--- DiscordUTC/DiscordUTC/Form1.Designer.cs
+++ DiscordUTC/DiscordUTC/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.CopyUTC = new System.Windows.Forms.Button();
+            this.timePicker = new System.Windows.Forms.DateTimePicker();
             this.SuspendLayout();
+            // 
+            // datePicker
+            // 
+            this.datePicker.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.datePicker.Location = new System.Drawing.Point(12, 12);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(200, 20);
+            this.datePicker.TabIndex = 0;
+            // 
+            // CopyUTC
+            // 
+            this.CopyUTC.Location = new System.Drawing.Point(5, 64);
+            this.CopyUTC.Name = "CopyUTC";
+            this.CopyUTC.Size = new System.Drawing.Size(200, 50);
+            this.CopyUTC.TabIndex = 1;
+            this.CopyUTC.Text = "Copy UTC Data";
+            this.CopyUTC.UseVisualStyleBackColor = true;
+            this.CopyUTC.Click += new System.EventHandler(this.CopyUTC_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.timePicker.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.timePicker.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.timePicker.Location = new System.Drawing.Point(12, 38);
+            this.timePicker.Name = "dateTimePicker1";
+            this.timePicker.Size = new System.Drawing.Size(200, 20);
+            this.timePicker.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(376, 203);
+            this.ClientSize = new System.Drawing.Size(217, 126);
+            this.Controls.Add(this.timePicker);
+            this.Controls.Add(this.CopyUTC);
+            this.Controls.Add(this.datePicker);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +75,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Button CopyUTC;
+        private System.Windows.Forms.DateTimePicker timePicker;
     }
 }
 
--- DiscordUTC/DiscordUTC/Form1.cs
+++ DiscordUTC/DiscordUTC/Form1.cs
@@ -12,9 +12,35 @@ namespace DiscordUTC
 {
     public partial class Form1 : Form
     {
+        DateTime epoch;
         public Form1()
         {
             InitializeComponent();
+            epoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            datePicker.ShowUpDown = true;
+            timePicker.ShowUpDown = true;
+        }
+
+        private void CopyUTC_Click(object sender, EventArgs e)
+        {
+            DateTime totalTime = datePicker.Value.Date + timePicker.Value.TimeOfDay;
+            double unixTimestamp = ConvertToUnixTimestamp(totalTime);
+            
+            string discordText = $"<t:{unixTimestamp}>";
+
+            Clipboard.SetText(discordText);
+        }
+        public static DateTime ConvertFromUnixTimestamp(double timestamp)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            return origin.AddSeconds(timestamp);
+        }
+
+        public static double ConvertToUnixTimestamp(DateTime date)
+        {
+            DateTime origin = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan diff = date.ToUniversalTime() - origin;
+            return Math.Floor(diff.TotalSeconds);
         }
     }
 }
